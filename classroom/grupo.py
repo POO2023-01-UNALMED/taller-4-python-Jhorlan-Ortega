f77f60b5153469b117af65bcba7741a3c6461a32
Jhorlan-Ortega <124930448+Jhorlan-Ortega@users.noreply.github.com>
2023-03-07 10:55:02 -0500

    commit_4

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas =[], estudiantes= []):
+    def __init__(self, grupo="grupo predeterminado", asignaturas =[], estudiantes= []):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
@@ -19,7 +19,7 @@ class Grupo:
         self.listadoAlumnos = self.listadoAlumnos + lista
 
      def __str__(self):
-         str = 'grupo de estudiantes '+self._grupo
+         str = 'Grupo de estudiantes '+self._grupo
          return str
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
